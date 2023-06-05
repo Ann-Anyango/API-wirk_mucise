8b8689b1a728dab5c043cc3af6028f3ce54de285
Ann-Anyango <annmwangombe@gmail.com>
2023-06-05 07:26:02 +0300

    second  commit

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const getUsers = () => {
-    return fetch('https://dummyjson.com/products/1')
-      .then(response => response.json())
-      .then(response => response)
-      .catch(error => error);
-  };
+function generateProductCards(data) {
+  const productContainer = document.getElementById("products");
+  data.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("product-card");
+    card.innerHTML = `
+            <img src="${product.thumbnail}" alt="${product.name}" />
+            <h2>${product.title}</h2>
+            <div class='productPrice'>
+            <p>${product.price}</p>
+            <button>ADD TO CART</button>
+            </div>
+        `;
+    productContainer.appendChild(card);
+  });
+}
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      const returnedData = data.products.slice(0, 8);
+      generateProductCards(returnedData);
+    });
+}
+fetchProducts()
